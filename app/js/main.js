4ff66c3432ab777fd2b604a93e1a5f557780cf2e
ArtyoumStar <momomo322@yandex.ru>
2022-11-29 00:07:20 +0300

    feat: add function open close rightside menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
-$(function () {
-    
-})
+const header__btn = document.querySelector('.header__btn');
+const rightside_menu = document.querySelector('.rightside-menu');
+const rightside_menu_close = document.querySelector('.rightside-menu__close');
+
+header__btn.addEventListener('click', () => {
+    rightside_menu.classList.remove('rightside-menu--close');
+});
+
+rightside_menu_close.addEventListener('click', () => {
+    rightside_menu.classList.add('rightside-menu--close');
+});;
